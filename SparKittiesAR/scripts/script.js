28c9389a2019050309816df5f5d9197921d475ec
daaab <dablog@gmail.com>
2019-05-31 17:36:40 +0800

    Modify for open source
    - Remove FB Example code
    - Add comment

--- SparKittiesAR/scripts/script.js
+++ SparKittiesAR/scripts/script.js
@@ -1,39 +1,8 @@
 //Header
-//Copyright 20__-present, Facebook, Inc.
-//All rights reserved.
-
-//This source code is licensed under the license found in the
-//LICENSE file in the root directory of this source tree.
 
 var Animation = require('Animation');
 var FaceTracking = require('FaceTracking');
 const Scene = require('Scene');
-
-var ft = Scene.root.child("Device").child("Camera").child("Focal Distance").child("facetracker0");
-
-var pizzaWheel0 = ft.child("pizzas_123");
-var pizzaWheel1 = ft.child("pizzas_456");
-var pizzaWheel2 = ft.child("pizzas_789");
-
-var mouthIsOpen = FaceTracking.face(0).mouth.openness.gt(0.3).and(FaceTracking.count.gt(0));
-
-pizzaWheel0.hidden = pizzaWheel1.hidden = pizzaWheel2.hidden = mouthIsOpen.not();
-
-var wheelDriver = Animation.timeDriver({durationMilliseconds: 1500, loopCount: Infinity});
-var wheelSampler = Animation.samplers.linear(0, -Math.PI*1);
-
-pizzaWheel0.transform.rotationX = Animation.animate(wheelDriver, wheelSampler);
-pizzaWheel1.transform.rotationX = Animation.animate(wheelDriver, wheelSampler);
-pizzaWheel2.transform.rotationX = Animation.animate(wheelDriver, wheelSampler);
-
-mouthIsOpen.monitor().subscribe( function(e) {
-  if (e.newValue == true) {
-    wheelDriver.start();
-  } else {
-  	wheelDriver.stop();
-  	wheelDriver.reset();
-  }
-});
 
 //==============================================================================
 // The following example demonstrates how to send a JSON request to an online
@@ -58,7 +27,17 @@ const Textures = require('Textures');
 //==============================================================================
 
 // Store the URL we're sending the request to
-const url = 'https://api.cryptokitties.co/kitties?owner_wallet_address=0x12a0E25E62C1dBD32E505446062B26AECB65F028&limit=4&offset=0';
+
+//==============================================================================
+// Fetch blockchain data
+//==============================================================================
+// load CryptoKitties by using CK API https://api.cryptokitties.co/kitties?owner_wallet_address=[YOUR-PUBLIC-ETH-WALLET-ADDRESS]&limit=[NUMBER-OF-KITTIES]&offset=[START-FROM-WHICH-KITTY]
+// you can also use OpenSea API https://api.opensea.io/api/v1/assets/?format=json&order_by=current_price&order_direction=asc&owner=[YOUR-PUBLIC-ETH-WALLET-ADDRESS]
+// also show how rich this user is by Etherscan API https://api.etherscan.io/api?module=account&action=balance&address=[YOUR-PUBLIC-ETH-WALLET-ADDRESS]
+// don't forget to Whitelist the api.cryptokitties.co or other api domain in Capabilities.
+// if you want to display Kitties PNG as externalTexure source, remember to Whitelist the img.cryptokitties.co as well.
+
+const url = 'https://api.cryptokitties.co/kitties?owner_wallet_address=0x12a0E25E62C1dBD32E505446062B26AECB65F028&limit=4&offset=1';
 
 // Create a request object
 const request = {
@@ -91,9 +70,12 @@ Networking.fetch(url, request).then(function(result) {
 }).then(function(json) {
 
   // Log the JSON obtained by the successful request
-  Diagnostics.log('Successfully sent - ' + json.kitties[0].image_url_png);
+  // JC: Sorry for some brute-force hard coding style...
   for (var i=0;i<3;i++){
+    //Check if data loaded
     Diagnostics.log(i);
+    Diagnostics.log('Successfully load - ' + json.kitties[i].image_url_png);
+    //Display Kitties
     Textures.get('externalTexture'+i).url = json.kitties[i].image_url_png;
   }
 }).catch(function(error) {
